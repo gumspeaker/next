2165b5aaf38778abea5f4a9b3020e96cb6e8b50c
gumspeaker <774910945@qq.com>
2023-04-18 00:12:44 +0800

    feat: sava

--- apps/app/src/pages/form/edit/components/Body.tsx
+++ apps/app/src/pages/form/edit/components/Body.tsx
@@ -1,16 +1,33 @@
 import { FC } from "react";
-import { useForm, FormProvider, useFormContext } from "react-hook-form";
+import {
+  useForm,
+  FormProvider,
+  useFormContext,
+  UseFormProps,
+} from "react-hook-form";
+import { Form } from "antd";
 
-function App() {
-  const methods = useFormContext();
-  return <form onSubmit={methods.handleSubmit(() => {})}></form>;
-}
-// type
-export default function Wrapper({ children }) {
-  const methods = useForm();
+type defaultValues = Record<string, any>;
+type RenderFormProps<VALUES extends defaultValues> = {
+  onSubmit?: (v: VALUES) => void;
+} & UseFormProps<VALUES, any>;
+
+function noon() {}
+
+export default function Wrapper<VALUES extends defaultValues>(
+  props: RenderFormProps<VALUES>
+) {
+  const { onSubmit = noon, defaultValues } = props;
+  const methods = useForm<VALUES>({ defaultValues });
+  const submit = methods.handleSubmit(onSubmit);
   return (
     <FormProvider {...methods}>
       <App></App>
     </FormProvider>
   );
+}
+
+function App() {
+  const methods = useFormContext();
+  return <form onSubmit={methods.handleSubmit(() => {})}></form>;
 }
